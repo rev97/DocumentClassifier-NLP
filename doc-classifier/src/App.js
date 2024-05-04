b921ffe8052e50794992ae4fe24df7ee1c74fcb2
rev97 <89413538+rev97@users.noreply.github.com>
2024-05-04 17:56:59 -0400

    Made changes for app routing between home and model training

--- doc-classifier/src/App.js
+++ doc-classifier/src/App.js
@@ -1,20 +1,39 @@
-import {useState} from "react";
-import FileInput from "./pages/FileInput";
-import Classification from "./pages/Classification";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FileInput from './pages/FileInput';
+import Classification from './pages/Classification';
+import TrainModel from "./pages/TrainModel";
+import TrainOutput from "./pages/TrainOutput";
 
 function App() {
     const [response, setResponse] = useState(null);
-    const resetResponse = () => {setResponse(null)};
-    return (
-        <div className="App">
-            {
-                response === null && <FileInput setResponse={setResponse}/>
-            }
+    const resetResponse = () => setResponse(null);
 
-            {
-                response !== null &&  <Classification response={response} resetResponse={resetResponse}/>
-            }
-        </div>
+    const [trainresponse, settrainresponse] = useState(null);
+    const resettrainresponse = () => settrainresponse(null);
+
+    return (
+        <Router>
+                <Routes>
+
+                    <Route
+                        path="/"
+                        element={ response === null ? (
+                            <FileInput setResponse={setResponse} />
+                        ) : (
+                            <Classification response={response} resetResponse={resetResponse} />
+                        )}
+                    />
+                    <Route
+                        path="/model"
+                        element={ trainresponse === null ? (
+                            <TrainModel setTrainresponse={settrainresponse} />
+                        ) : (
+                            <TrainOutput trainresponse={trainresponse} resetTrainresponse={resettrainresponse} />
+                        )}
+                    />
+                </Routes>
+        </Router>
     );
 
 }
