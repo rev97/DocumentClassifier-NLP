cf90fd076e0d0f7f7eae70fdc3128980eaf1c4ee
rev97 <89413538+rev97@users.noreply.github.com>
2024-05-05 20:02:00 -0400

    added exceptional handling in train_model.py

--- backend/backend/train_model.py
+++ backend/backend/train_model.py
@@ -29,6 +29,8 @@ class TextClassifier:
         lemmatizer = nltk.stem.WordNetLemmatizer()
         lemmatized_text = ' '.join(
             [lemmatizer.lemmatize(word) for word in cleaned_text.split() if word not in self.stop_words])
+        if not lemmatized_text.strip():  # Check if the processed text is empty
+            return None  # Return None if the text becomes empty after preprocessing
         return lemmatized_text
 
     def extract_keywords_nltk(self, text, user_keywords):
@@ -36,23 +38,35 @@ class TextClassifier:
         return " ".join(keywords)
 
     def train_model(self, user_keywords):
-        X = [self.preprocess_text(text) for text in self.dataset_df['text']]
-        X_keywords = [self.extract_keywords_nltk(text, user_keywords) for text in X]
-        y = self.dataset_df['Label']
+        try:
+            X = [self.preprocess_text(text) for text in self.dataset_df['text'] if self.preprocess_text(text)]
+            if not any(X):  # Check if all processed texts are empty or None
+                raise ValueError("No valid documents available for training")
 
-        X_train, X_test, y_train, y_test = train_test_split(X_keywords, y, test_size=0.2, random_state=42)
+            X_keywords = [self.extract_keywords_nltk(text, user_keywords) for text in X]
+            y = self.dataset_df['Label']
 
-        model = make_pipeline(
-            TfidfVectorizer(sublinear_tf=True, encoding='utf-8', decode_error='ignore', stop_words='english',
-                            analyzer='word', norm='l2', lowercase=True), MultinomialNB())
+            X_train, X_test, y_train, y_test = train_test_split(X_keywords, y, test_size=0.2, random_state=42)
 
-        model.fit(X_train, y_train)
+            if not any(X_train):  # Check if all training data is empty after processing
+                raise ValueError("No valid documents available for training")
 
-        y_pred = model.predict(X_test)
+            model = make_pipeline(
+                TfidfVectorizer(sublinear_tf=True, encoding='utf-8', decode_error='ignore', stop_words='english',
+                                analyzer='word', norm='l2', lowercase=True), MultinomialNB())
 
-        accuracy = metrics.accuracy_score(y_test, y_pred)
-        print(f"Accuracy: {accuracy}")
 
-        return model, y_pred
+            model.fit(X_train, y_train)
+
+            y_pred = model.predict(X_test)
+
+            accuracy = metrics.accuracy_score(y_test, y_pred)
+            print(f"Accuracy: {accuracy}")
+
+            return model, y_pred
+
+        except ValueError as e:
+            print(f"Error occurred while fitting the model: {e}")
+            return None, None  # Return None if model fitting fails
 
 
